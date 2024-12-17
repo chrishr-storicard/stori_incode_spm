5db54fa6a23b61fe8240eb3f37f1dd34811060a7
chrishr-storicard <christian.hernandez@storicard.com>
2024-12-17 10:53:02 -0600

    Auto stash before rebase of "main" onto "origin/main"

--- StoriIncodeSPM/Package.swift
+++ StoriIncodeSPM/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "StoriIncodeSPM",
             url: "https://github.com/chrishr-storicard/stori_incode_spm/releases/download/1.0.0/IncdOnboarding.xcframework.zip",
-            checksum: "bd769b2180d6c89a38ef6d85997ceaa86a01704bc607fdb7674ff16d27b4ab92"
+            checksum: "a69606849c3c60b1c6098920aca7b22a8bb7f9e69b3367fc72d69c90d323f8f5"
         )
     ]
 )
