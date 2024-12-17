e794fb7deabc751e6bccc43814a08fe67d9b3923
chrishr-storicard <christian.hernandez@storicard.com>
2024-12-16 21:32:27 -0600

    changes

--- StoriIncodeSPM/Package.swift
+++ StoriIncodeSPM/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "StoriIncodeSPM",
-            url: "https://github.com/chrishr-storicard/stori_incode_spm/releases/download/v1.0.0/IncdOnboarding.xcframework.zip",
+            url: "https://github.com/chrishr-storicard/stori_incode_spm/releases/download/1.0.0/IncdOnboarding.xcframework.zip",
             checksum: "bd769b2180d6c89a38ef6d85997ceaa86a01704bc607fdb7674ff16d27b4ab92"
         )
     ]
